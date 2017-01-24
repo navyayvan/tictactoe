58b0e96098eccd8fdfe1f5784e5e120ac4fdbdb7
navyayvan <navyayvan@gmail.com>
2017-01-23 18:32:01 -0800

    added gameboard, added player and play move functionality

---
+++ script.js
@@ -0,0 +1,55 @@
+$(document).ready( function() { 
+
+  var count = 0;
+  var move;
+
+  var one = document.getElementById("1");
+  var two = document.getElementById("2");
+  var three = document.getElementById("3");
+  var four =document.getElementById("4");
+  var five = document.getElementById("5");
+  var six = document.getElementById("6");
+  var seven = document.getElementById("7");
+  var eight = document.getElementById("8");
+  var nine = document.getElementById("9");
+  var reset = document.getElementById("reset");
+  var whichPlayer = document.getElementById("whichPlayer");
+
+  var arrayTiles = [one, two, three, four, five, six, seven, eight, nine];
+  var len = arrayTiles.length;
+
+  function player() {
+    if (count === 0) {
+       move = 'x';
+       whichPlayer.innerHTML = 'Player 2';
+    } else if ( (count/2) % 1){
+      move = 'o';
+      whichPlayer.innerHTML = 'Player 1';
+    } else {
+      move = 'x';
+      whichPlayer.innerHTML = 'Player 2';
+    }
+    count++;
+  }
+
+  function play() {
+    player();
+    this.innerHTML = move;
+  }
+
+  for (var i = 0; i < len; i++) {
+    arrayTiles[i].addEventListener("click", play);
+  }
+
+  reset.addEventListener("click", function() {
+    for (var j = 0; j < len; j++) {
+      arrayTiles[j].innerHTML = '';
+      whichPlayer.innerHTML = 'Player 1 begins...';
+    }
+  });
+
+
+});
+
+
+
